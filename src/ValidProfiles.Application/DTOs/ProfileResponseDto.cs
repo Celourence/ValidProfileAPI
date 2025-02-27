225dc190d6eb09e01ed0d9ac098dda84ad3ace55
Christian <celourence@gmail.com>
2025-02-26 23:06:35 -0300

    Fix DTOs to support boolean parameters and add Swagger documentation

--- src/ValidProfiles.Application/DTOs/ProfileResponseDto.cs
+++ src/ValidProfiles.Application/DTOs/ProfileResponseDto.cs
@@ -1,21 +1,18 @@
-using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace ValidProfiles.Application.DTOs;
 
 /// <summary>
-/// DTO para criação de perfil
+/// DTO de resposta para perfil
 /// </summary>
-public class ProfileDto
+public class ProfileResponseDto
 {
     /// <summary>
     /// Nome do perfil
     /// </summary>
-    [Required(ErrorMessage = "O nome do perfil é obrigatório")]
-    [StringLength(50, ErrorMessage = "O nome do perfil deve ter no máximo 50 caracteres")]
     [JsonPropertyName("profileName")]
-    [SwaggerSchema("Nome do perfil", Nullable = false)]
+    [SwaggerSchema("Nome do perfil")]
     public required string Name { get; set; }
 
     /// <summary>
@@ -24,4 +21,4 @@ public class ProfileDto
     [JsonPropertyName("parameters")]
     [SwaggerSchema("Parâmetros do perfil")]
     public Dictionary<string, string> Parameters { get; set; } = new();
-}
+} 
